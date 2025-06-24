0aa4cda937ca2a8830a62c964e053af6296fe202
박현창 <hyunchang@ponylink.co.kr>
2025-06-24 18:30:37 +0900

    모바일로 볼시에 오른쪽으로 쏠림현상 수정

--- src/App.vue
+++ src/App.vue
@@ -24,4 +24,10 @@ export default {
   padding-top: 51px; /* header/nav 높이만큼 조정 */
   padding-left: 75px; /* 사이드 메뉴의 width와 동일하게 */
 }
+@media (max-width: 800px) {
+  .container {
+    padding-top: 0;
+    padding-left: 15px;
+  }
+}
 </style>
