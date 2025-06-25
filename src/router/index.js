4b3be4fb042e6c16d23df136d199c64744ea521b
박현창 <hyunchang@ponylink.co.kr>
2025-06-25 10:04:18 +0900

    모바일에서 메뉴 이동후 새로고침시에 404 로 넘어가서 Vue Router를 hash mode로 변경

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import Home from '../pages/index.vue';
 import History from '../pages/history.vue';
 import Todos from '../pages/todos/index.vue';
@@ -8,7 +8,7 @@ import Count from '../pages/count.vue';
 import Projects from '../pages/projects.vue';
 
 const router = createRouter({
-    history: createWebHistory(),
+    history: createWebHashHistory(),
     routes: [
         {
             path: '/',
